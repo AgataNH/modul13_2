b0c67ae801ea3bbdde0d4fc9462f745ae16036a8
Agata Niedolistek <a.niedolistek@ibe.edu.pl>
2022-07-14 11:00:32 +0200

    add import

--- add_data.py
+++ add_data.py
@@ -1,19 +1,6 @@
 import sqlite3
 from sqlite3 import Error
-
-def create_connection(db_file):
-   """ create a database connection to the SQLite database
-       specified by db_file
-   :param db_file: database file
-   :return: Connection object or None
-   """
-   conn = None
-   try:
-       conn = sqlite3.connect(db_file)
-       return conn
-   except Error as e:
-       print(e)
-   return conn
+from create_tables import create_connection
 
 def add_meeting(conn, meeting):
    """
